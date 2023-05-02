e8397e2249ee67160721de34e0667f7a9dc028bc
leonard <mortuza.7@gmail.com>
2023-05-03 02:43:38 +0600

    home page few section added

--- src/components/FoodMeu.jsx
+++ src/components/FoodMeu.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import menuImg from '../assets/food-knife.png'
 const FoodMeu = () => {
     return (
-        <div className="my-5">
+        <div className="my-1">
             <div  className="flex justify-center">
                 <img src={menuImg} className="w-20 -mb-5" alt="" />
             </div>
             <h4 className='text-4xl text-center mb-5'>Our Menu</h4>
             <div className="flex flex-col md:flex-row gap-4">
                 <div className="card w-96 glass">
-                    <figure  className="h-64"><img className="w-96" src="https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900" alt="car!" /></figure>
+                    <figure  className="h-52"><img className="w-96" src="https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900" alt="car!" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Pizza</h2>
                         <p>Pizza is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients, which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta.</p>
@@ -19,7 +19,7 @@ const FoodMeu = () => {
                     </div>
                 </div>
                 <div className="card w-96 glass">
-                    <figure className="h-64"><img src="https://cdn.loveandlemons.com/wp-content/uploads/2019/07/salad.jpg" alt="car!" /></figure>
+                    <figure className="h-52"><img src="https://cdn.loveandlemons.com/wp-content/uploads/2019/07/salad.jpg" alt="car!" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Salad</h2>
                         <p>The five basic types of salad are green salads (tossed or composed), bound, vegetable, fruit, and combination. The five basic salads that can be served throughout the course of a meal are starter, accompaniment, main course, intermezzo, and dessert.</p>
@@ -29,7 +29,7 @@ const FoodMeu = () => {
                     </div>
                 </div>
                 <div className="card w-96 glass">
-                    <figure  className="h-64"><img src="https://cookingchew.com/wp-content/uploads/2022/01/Fancy-Desserts_1-min.jpg" alt="car!" /></figure>
+                    <figure  className="h-52"><img src="https://cookingchew.com/wp-content/uploads/2022/01/Fancy-Desserts_1-min.jpg" alt="car!" /></figure>
                     <div className="card-body">
                         <h2 className="card-title">Desserts</h2>
                         <p>Cheesecake, cupcakes, jell-O, carrot cake, apple pie, ice cream, brownies, chocolate chip cookies and chocolate cake are the most popular desserts in Italy.</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
 import FoodMeu from '../components/FoodMeu';
+import Catering from '../components/Catering';
 
 const Home = () => {
     return (
         <div>
             <Carousel></Carousel>
             <FoodMeu></FoodMeu>
+            <Catering></Catering>
         </div>
     );
 };
