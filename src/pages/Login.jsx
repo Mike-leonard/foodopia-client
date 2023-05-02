7b766e1b9bc12d9d27b82a30ccccdee07b90a2c3
leonard <mortuza.7@gmail.com>
2023-05-02 21:47:02 +0600

    login and register page design completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="hero bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold">Login now!</h1>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -28,7 +28,13 @@ const Login = () => {
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
                         </div>
-                    </div>
+                    </form>
+                    {/* TODO: Error message need to show */}
+                    <span className="text-center mb-3 -mt-4">New to Foodopia? <Link to="/register" className="text-red-600">Register</Link></span>
+                </div>
+                <div className="flex gap-4">
+                    <button className="btn btn-info text-primary">Google Login</button>
+                    <button className="btn btn-info text-primary">Github Login</button>
                 </div>
             </div>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="hero bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-3xl font-bold">Register now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    {/* TODO: passsword validation needed */}
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="text" placeholder="password" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">PhotoURL</span>
+                            </label>
+                            <input type="text" placeholder="Photo URL" className="input input-bordered" />
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                    {/* TODO: Error message need to show */}
+                    <span className="text-center mb-3 -mt-4">
+                        Already have an account? <Link to="/login" className="text-red-600">Login</Link>
+                    </span>
+                </div>
+            </div>
         </div>
     );
 };
